56025b68fe263e48a92ffb59cfb76ed07fd3f1d7
LitterB <wenhuakailong@163.com>
2019-08-07 13:04:29 +0800

    添加了lxml安装地址及版本说明

--- spider-wechat-response/index.py
+++ spider-wechat-response/index.py
@@ -1,6 +1,8 @@
 
-# 安装lxml，需先安装wheel,然后下载指定的lxml包，进行本地安装，lxml的包需要对应python的版本号及位数
+#安装lxml，需先安装wheel,然后下载指定的lxml包，进行本地安装，lxml的包需要对应python的版本号及位数
 #python 安装的为32位程序则下载32位的包，并不是按照操作系统的位数，64位操作系统32位python，所需lxml版本为32位版本
+#下载地址 https://www.lfd.uci.edu/~gohlke/pythonlibs/#lxml
+# python 3.7 32位 OS 64位 对应版本为 lxml‑4.3.5‑cp37‑cp37m‑win32.whl
 
 
 import requests
